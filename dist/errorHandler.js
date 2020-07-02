9615e3c2d0ab571792a2d27afe1ee8c8904409fd
Georgi Yazovaliyski <georgi.yazovaliiski@gmail.com>
2020-07-02 17:52:52 +0300

    remove build from repo

--- dist/errorHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const next = (err) => {
-    if (err)
-        return console.log('watch failed: ', err.message);
-    console.log('watch successful!');
-};
-exports.default = next;
-//# sourceMappingURL=errorHandler.js.map
